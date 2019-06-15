99ac32629ab9e061f60ff5a7a34d0a0e3fc0c168
cecilesauder <cecile.sauder@gmail.com>
2019-06-16 01:49:35 +0200

    structure app

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,11 @@
-#' @export
+#' Launch the app
+#'
+#'  @export
 run_app <- function() {
   appDir <- system.file("shiny-app", "BibliographeR", package = "bibliographeR")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `bibliographeR`.", call. = FALSE)
+    stop("Could not find application directory. Try re-installing `bibliographeR`.", call. = FALSE)
   }
-  
+
   shiny::runApp(appDir, display.mode = "normal")
 }
